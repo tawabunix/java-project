f7faec622a53ec4b40aeec9f0005ddc34a27ef98
tawabunix <tawab.unix@gmail.com>
2018-07-26 17:43:45 -0400

    commit 2

---
+++ src/Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle {
+
+public int length;
+public int width;
+
+public Rectangle(int length, int width) {
+this.lenth = length;
+this.width = width;
+}
+public int getArea() {
+return lenth * width;
+}
+
+public int getParameter() {
+
+return 2 * (length + width);
+
+}
+
+}
---
+++ src/Rectangulator.java
@@ -0,0 +1,14 @@
+public class Rectangulator {
+     public static void main (string[] args) {
+        int length = Integer.parseInt(args[0]);
+          int width = Integer.parseInt(args[1]);
+
+          Rectangle myRectangle = new Rectangle(length, width);
+
+          String output = String.format("*** your Rectangle ***\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", MyRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+		  
+		  system.out.println(output);
+
+       }
+
+)
